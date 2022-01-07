6b7f518a24b834072af64fbdadac5f39ac2897ba
MichaelAbe <michael.k.abe@gmail.com>
2022-01-06 20:58:05 -0500

    exporting components in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export { default as Navbar } from './Navbar'
+export { default as Cryptocurrnecies } from './Cryptocurrnecies'
+export { default as CryptoDetails } from './CryptoDetails'
+export { default as Exchanges } from './Exchanges'
+export { default as Homepage } from './Homepage'
+export { default as News } from './News'
