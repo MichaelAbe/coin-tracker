0d9dd587a0fce6f4aa316773faf1a204e433ec90
MichaelAbe <michael.k.abe@gmail.com>
2022-01-07 11:19:31 -0500

    homepage statistic setup

--- src/App.js
+++ src/App.js
@@ -17,19 +17,26 @@ const App = () => {
                 <Layout>
                     <div className='routes'>
                         <Routes>
-                        <Route path='/' element={<Homepage/>} />
-                        <Route path='/exchanges' element={<Exchanges/>} />
-                        <Route path='/cryptocurrnecies' element={<Cryptocurrencies />} />
-                        <Route path='/news' element={<News/>} />
-                        <Route path='/crypto/:coinId' element={<CryptoDetails />} />
-                         
+                            <Route path='/' element={<Homepage/>} />
+                            <Route path='/exchanges' element={<Exchanges/>} />
+                            <Route path='/cryptocurrnecies' element={<Cryptocurrencies />} />
+                            <Route path='/news' element={<News/>} />
+                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
                         </Routes>
                     </div>
                 </Layout>
+
+            <div className='footer' >
+                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                    Coin-Tracker <br />
+                    All Rights Reserved 2022
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
             </div>
-
-            <div className='footer'>
-
             </div>
          
         </div>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
+
+const { Title } = Typography 
+// to make it easier instead of typing 'Typography.Title'
 
 function Homepage() {
     return (
-        <div>
-            HomePage
-        </div>
+        <>
+            <Title level={2} className='heading'>Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrnecies" value='5'/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value='5'/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value='5'/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value='5'/></Col>
+                <Col span={12}><Statistic title="Total Markets" value='5'/></Col>
+            </Row>
+        </>
     )
 }
 
